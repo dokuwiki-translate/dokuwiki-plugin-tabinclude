96bc08741705b45f0cac691260d1c964a631e8c7
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:56:50 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['use_first_heading']     = 'Použiť prvý nadpis v tabe';
+$lang['namespace_in_tab']      = 'Zahrnúť menné priestory do ID stránky';
+$lang['hideloading']           = 'Skryť správu o načítavaní (AJAX režim)';
+$lang['ajax_init_page']        = 'Načítať úvodnú stránku tabu cez AJAX (AJAX režim)';
+$lang['goto_link_header']      = 'Zobraziť odkaz na stránku v hlavičke';
+$lang['goto_link_footer']      = 'Zobraziť odkaz na stránku v pätičke';
+$lang['toc_in_tab']            = 'Zobraziť obsah (TOC) v tab stránkach';
